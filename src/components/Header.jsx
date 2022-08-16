4ce05a1338519ce16c642d36384d2ffc590475d9
Junior MILIEN <juniorthx@gmail.com>
2022-08-16 01:33:54 -0400

    Improve Header and fix mobile size

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,12 +1,19 @@
 import {Link} from 'react-router-dom'
 import logo from '../images/logo.svg'
-import {GiHamburgerMenu} from 'react-icons/gi'
-import {GrClose} from 'react-icons/gr'
-import {IoIosArrowDown} from "react-icons/io"
 import { useState } from 'react'
+import arrowDown from '../images/icon-arrow-down.svg'
+import iconClose from '../images/icon-close-menu.svg'
+import iconMenu from '../images/icon-menu.svg'
+import iconTodo from '../images/icon-todo.svg'
+import iconPlanning from '../images/icon-planning.svg'
+import iconReminders from '../images/icon-reminders.svg'
+import iconCalendar from '../images/icon-calendar.svg'
 
 const Header = () => {
   const [open, setOpen] = useState(false)
+  const [openSub, setOpenSub] = useState(false)
+  const [openSub2, setOpenSub2] = useState(false)
+
   return (
     <nav className='relative mx-5 p-5'>
       <div className="flex items-center space-x-10">
@@ -15,8 +22,29 @@ const Header = () => {
         </div>
         <div className="hidden w-screen md:flex items-center justify-between text-mediumGray">
            <div className='space-x-10 text-sm'>
-              <Link to="/features" className="hover:text-almostBlack ">Features <IoIosArrowDown className='inline'/></Link>
-              <Link to="/company" className="hover:text-almostBlack">Company  <IoIosArrowDown className='inline' /></Link>
+              <Link to="/features" className="hover:text-almostBlack" onClick={()=>setOpenSub(!openSub)}>Features <img src={arrowDown} alt="" className="inline" />
+              {
+                openSub && (
+                  <div className='absolute top-10 bg-gray-200 flex flex-col rounded mt-3 p-3 text-sm w-32 space-y-2'>
+                    <Link to="/" className="hover:text-almostBlack"><img src={iconTodo} alt="" className="inline p-1" /> Todo List</Link>
+                    <Link to="/" className="hover:text-almostBlack"><img src={iconCalendar} alt="" className="inline p-1" /> Calendar</Link>
+                    <Link to="/" className="hover:text-almostBlack"><img src={iconReminders} alt="" className="inline p-1" /> Reminders</Link>
+                    <Link to="/" className="hover:text-almostBlack"><img src={iconPlanning} alt="" className="inline p-1" /> Planning</Link>
+                  </div>
+                )
+              }
+              </Link>
+              <Link to="/company" className="hover:text-almostBlack" onClick={()=>setOpenSub2(!openSub2)}>Company <img src={arrowDown} alt="" className="inline" />
+              {
+                openSub2 && (
+                  <div className='absolute top-10 bg-gray-200 flex flex-col rounded mt-3 p-3 text-sm w-32 space-y-2'>
+                    <Link to="/" className="hover:text-almostBlack">History</Link>
+                    <Link to="/" className="hover:text-almostBlack">Our Team</Link>
+                    <Link to="/" className="hover:text-almostBlack">Blog</Link>
+                  </div>
+                )
+              } 
+              </Link>
               <Link to="/careers" className="hover:text-almostBlack">Careers</Link>
               <Link to="/about" className="hover:text-almostBlack">About</Link>
            </div>
@@ -27,15 +55,15 @@ const Header = () => {
         </div>
         <button id="menu-btn" onClick={()=>setOpen(!open)} className="absolute right-0 focus:outline-none md:hidden">
               {
-                open ? <GrClose size="2rem" /> : <GiHamburgerMenu size="2rem" />
+                open ? <img src={iconClose} alt="" /> : <img src={iconMenu} alt="" />
               }
         </button> 
         {
           open && (
             <div className="md:hidden">
                 <div id="menu" className="text-sm absolute flex flex-col items-start px-5 self-end text-mediumGray py-8 mt-5 space-y-6 bg-almostWhite sm:w-auto sm:self-start w-1/2 h-screen left-1/2 -right-5 drop-shadow-md">
-                    <Link to="/features" className="hover:text-almostBlack">Features <IoIosArrowDown className='inline'/></Link>
-                    <Link to="/company" className="hover:text-almostBlack">Company <IoIosArrowDown className='inline'/></Link>
+                    <Link to="/features" className="hover:text-almostBlack">Features <img src={arrowDown} alt="" className="inline" /></Link>
+                    <Link to="/company" className="hover:text-almostBlack">Company <img src={arrowDown} alt="" className="inline" /></Link>
                     <Link to="/careers" className="hover:text-almostBlack">Careers</Link>
                     <Link to="/about" className="hover:text-almostBlack">About</Link>
                     <a href="/" className=" text-sm mx-auto hover:text-almostBlack">Login</a>
--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -10,14 +10,14 @@ const Hero = () => {
     <section className='md:mx-32'>
         <div className="flex flex-col-reverse justify-evenly md:flex-row">
             <div className="max-w-md flex flex-col justify-center md:w-1/2">
-                <h1 className="text-4xl font-bold text-center md:text-6xl md:text-left md:mt-20 md:mb-8">
+                <h1 className="mt-10 text-4xl font-bold text-center md:text-6xl md:text-left md:mt-20 md:mb-8">
                     Make <span className='inline md:block'>remote work</span>
                 </h1>
-                <p className='flex items-center text-sm text-center text-mediumGray md:mb-10 md:text-lg md:text-left'>
+                <p className='mt-5 md:mt-0 flex items-center text-sm text-center text-mediumGray md:mb-10 md:text-lg md:text-left'>
                     Get your team in sync, no matter your location. Streamline processes, 
                     create team rituals, and watch productivity soar.
                 </p>
-                <div className="flex items-center md:mb-20">
+                <div className="mx-auto mt-5 md:mt-0 flex items-center md:mx-0 md:mb-20">
                     <a href="/" className="bg-black text-almostWhite px-6 py-4 text-sm rounded-xl baseline hover:bg-almostWhite hover:border hover:border-black hover:text-black hover:font-bold">Learn more</a>
                 </div>  
                 <div className='space-x-10 flex items-center pt-10'>
